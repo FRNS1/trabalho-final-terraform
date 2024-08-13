bb6109a3b9b01fcf475c1930dd983da0112ea1cd
joaofernandesnovigo <joao.ferreira@novigo-it.com>
2024-08-13 20:26:17 -0300

    up

--- 02-Terraform/resize.sh
+++ 02-Terraform/resize.sh
@@ -1,46 +1,29 @@
 #!/bin/bash
 
-# Specify the desired volume size in GiB as a command-line argument. If not specified, default to 50 GiB.
+# Specify the desired volume size in GiB as a command-line argument. If not specified, default to 20 GiB.
 SIZE=50
 
-# Install necessary packages.
-sudo apt-get update
-sudo apt-get install -y jq awscli cloud-guest-utils
+# Install the jq command-line JSON processor.
+sudo apt install -y jq
 
-# Get the ID of the environment host Amazon EC2 instance.
-INSTANCEID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+# Get the ID of the envrionment host Amazon EC2 instance.
+INSTANCEID=$(curl http://169.254.169.254/latest/meta-data//instance-id)
 
 # Get the ID of the Amazon EBS volume associated with the instance.
-VOLUMEID=$(aws ec2 describe-instances --instance-ids $INSTANCEID --query "Reservations[0].Instances[0].BlockDeviceMappings[0].Ebs.VolumeId" --output text)
-
-if [ -z "$VOLUMEID" ]; then
-  echo "Error: Unable to retrieve the volume ID."
-  exit 1
-fi
+VOLUMEID=$(aws ec2 describe-instances --instance-id $INSTANCEID | jq -r .Reservations[0].Instances[0].BlockDeviceMappings[0].Ebs.VolumeId)
 
 # Resize the EBS volume.
 aws ec2 modify-volume --volume-id $VOLUMEID --size $SIZE
 
 # Wait for the resize to finish.
-echo "Waiting for volume modification to complete..."
-while true; do
-    STATUS=$(aws ec2 describe-volumes-modifications --volume-id $VOLUMEID --query "VolumesModifications[0].ModificationState" --output text)
-    
-    if [ "$STATUS" == "completed" ] || [ "$STATUS" == "optimizing" ]; then
-        echo "Volume modification completed."
-        break
-    elif [ -z "$STATUS" ]; then
-        echo "Error: Unable to retrieve the modification status."
-        exit 1
-    fi
-    
-    echo "Waiting for volume to be resized..."
-    sleep 5
-done
+while [ "$(aws ec2 describe-volumes-modifications --volume-id $VOLUMEID --filters Name=modification-state,Values="optimizing","completed" | jq '.VolumesModifications | length')" != "1" ]; do
+	  echo "waiting volume..."
+	  sleep 1
+  done
 
-# Rewrite the partition table so that the partition takes up all the space that it can.
-sudo lsblk
-sudo growpart /dev/xvda 1
+  # Rewrite the partition table so that the partition takes up all the space that it can.
+  lsblk
+  sudo growpart /dev/xvda 1
 
-# Expand the size of the file system.
-sudo resize2fs /dev/xvda1
+  # Expand the size of the file system.
+  sudo resize2fs /dev/xvda1 
